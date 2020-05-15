daf87b0eac7a9602a8e35ac18ce9351aaa358c5d
AmanGupta03 <theamangupta27@gmail.com>
2020-05-16 01:18:30 +0530

    fixing bugs

--- update.py
+++ update.py
@@ -50,7 +50,7 @@ new_scrapped_url = [row['url'] for row in data]
 embedding = [[float(x) for x in row['embedding'].split()] for row in data] #embeddings as list of list, ordered according to new_scrapped url
 
 #get pandas dataframe indexed on url to append on global_data
-data = newdomains.data_to_append(data, cur_date) 
+data = newdomains.data_to_append(data, str(cur_date)) 
 
 
 print('performing updates on global_data.....')
